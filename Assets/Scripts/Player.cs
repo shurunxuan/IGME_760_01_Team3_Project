72043b2d6eacd79943d84875e81e15dee2569659
Victor Shu <shurunxuan@hotmail.com>
2018-09-20 17:28:56 -0400

    Update player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,43 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-
-    private Vector3 _targetPosition;
+public class Player : MonoBehaviour
+{
+    public float Speed;
     private Rigidbody _rigidbody;
-
-	void Start ()
-	{
-	    _targetPosition = transform.position;
-	    _rigidbody = gameObject.GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    _targetPosition.y = transform.position.y;
-        if (Input.GetKey(KeyCode.A))
-        {
-            _targetPosition = new Vector3(_targetPosition.x, transform.position.y, _targetPosition.z - 15.0f * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _targetPosition = new Vector3(_targetPosition.x, transform.position.y, _targetPosition.z + 15.0f * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _targetPosition = new Vector3(_targetPosition.x + 15.0f * Time.deltaTime, transform.position.y, _targetPosition.z);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            _targetPosition = new Vector3(_targetPosition.x - 15.0f * Time.deltaTime, transform.position.y, _targetPosition.z);
-        }
-    }
-
-    void FixedUpdate()
+    // Use this for initialization
+    void Start()
     {
-        _rigidbody.MovePosition(_targetPosition);
+        // Get Rigidbody
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // Calculate the force from user input, and normalize it (making it just a direction)
+        Vector3 force = horizontal * Vector3.forward + vertical * Vector3.left;
+        force.Normalize();
+
+        // Create a ray from bottom of the object to the direction of force
+        Vector3 originPoint = transform.position - transform.localScale.y * Vector3.up;
+        Ray ray = new Ray(originPoint + 0.1f * Vector3.up, force);
+        RaycastHit raycastHit;
+
+        // Cast a ray toward the direction of force
+        bool hit = Physics.Raycast(ray, out raycastHit, 1.0f);
+        // If it hits nothing then open the gravity of object
+        _rigidbody.useGravity = true;
+        if (hit)
+        {
+            if (raycastHit.transform.name == "Terrain")
+            {
+                // If raycast hit the terrain then turn off the gravity of object
+                _rigidbody.useGravity = false;
+                // Make the force go through the hit point (and normalize it)
+                force = raycastHit.point - originPoint;
+                force.Normalize();
+            }
+        }
+
+        // Apply the force
+        _rigidbody.AddForce(force * Speed);
+    }
+
 }

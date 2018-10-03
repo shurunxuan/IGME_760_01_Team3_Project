3ea6e069f96a4016a4f0acb0eb305acc94599144
Runxuan Shu <shurunxuan@hotmail.com>
2018-10-03 16:34:12 -0400

    added a placeholder for a star unit

---
+++ Assets/Scripts/AStarUnit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarUnit : Unit
+{
+    private Rigidbody _rigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        // Get Rigidbody
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveUnit(_rigidbody);
+    }
+
+    // This is the direction the unit will head to when GetMoveDirection().x > 0
+    protected override void SetLocalRight()
+    {
+        localRight = Vector3.right;
+    }
+
+    // This is the direction the unit will head to when GetMoveDirection().y > 0
+    protected override void SetLocalForward()
+    {
+        localForward = Vector3.forward;
+    }
+
+    protected override Vector2 GetMoveDirection()
+    {
+        // TODO: implement the algorithm
+        return new Vector2(-1, 0);
+    }
+}

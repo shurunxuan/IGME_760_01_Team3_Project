e47ae0ff62550cbb14d27fc399939b84a9df020b
Runxuan Shu <shurunxuan@hotmail.com>
2018-10-12 11:17:13 -0400

    Add A* grid layout

---
+++ Assets/Scripts/GridNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode
+{
+
+    public Vector3 PositionInWorld;
+    public bool IsWalkable;
+    public int X;
+    public int Y;
+    public GridNode Parent;
+
+    public int gCost;
+    public int hCost;
+
+    public int walkstatus;
+
+    public GridNode(bool isWalkable, Vector3 position, int x, int y)
+    {
+        IsWalkable = isWalkable;
+        PositionInWorld = position;
+        X = x;
+        Y = y;
+    }
+}
